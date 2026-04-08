36133c5d559810ac4d286b574d3ed4bd8763a33b
Grenaderius <nazarkorbutak@gmail.com>
2026-04-08 15:51:59 +0300

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 
@@ -8,15 +8,14 @@ import UniqueNamesCreatorPrivacy from "./pages/privacy/UNC/UniqueNamesCreatorPri
 
 function App() {
     return (
+
         <Router>
             <Navbar />
-
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/privacy/UniqueNamesCreator" element={<UniqueNamesCreatorPrivacy />} />
                 <Route path="/app/UniqueNamesCreator" element={<UniqueNamesCreatorApp />} />
             </Routes>
-
             <Footer />
         </Router>
     );
--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+    base: '/grenaderius-official-site-with-android-apps-and-privacies/',
+    plugins: [
+        react({
+            babel: {
+                plugins: [['babel-plugin-react-compiler']],
+            },
+        }),
+    ],
 })
